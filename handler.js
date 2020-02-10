d7f60fb3686113caaff8c197a3999db78724bbe0
bitsbybyun <dev.younghb@gmail.com>
2020-02-09 22:15:44 -0800

    test commit

--- handler.js
+++ handler.js
@@ -5,7 +5,7 @@ module.exports.api = async event => {
     statusCode: 200,
     body: JSON.stringify({
         message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
+        value: 30
       },
       null,
       2
